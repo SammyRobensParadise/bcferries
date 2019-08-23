7bf5c70649fbea47c9158462aa9af3bc6a0c3a1e
Sammy Robens-Paradise <srobensparadise@gmail.com>
2019-08-22 20:55:45 -0700

    added the default class type for the app

--- bc-ferries-app/src/App.js
+++ bc-ferries-app/src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import React, {
+  Component
+} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class AppHandler extends Component {
+constructor(props){
+  super(props);
+  this.state = {
+    isActive: true
+  }
 }
-
-export default App;
+render(){
+  return(
+    <div>
+      <p>This is an  example of the default class</p>
+    </div>
+  )
+}
+}
--- bc-ferries-app/src/reducers/functions/index.js
+++ bc-ferries-app/src/reducers/functions/index.js
@@ -1,4 +1,7 @@
 const functions = require('firebase-functions');
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require('firebase-admin');
+admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
