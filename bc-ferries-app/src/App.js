e76d608baa9eeeefbc5558ca305886f0be7ee79d
Sammy Robens-Paradise <srobensparadise@gmail.com>
2019-08-22 21:02:47 -0700

    updated package.json

--- bc-ferries-app/src/App.js
+++ bc-ferries-app/src/App.js
@@ -3,12 +3,13 @@ import './App.css';
 import React, {
   Component
 } from 'react';
+import { BrowserRouter, Route, Link } from "react-router-dom";
 
 export default class AppHandler extends Component {
 constructor(props){
   super(props);
   this.state = {
-    isActive: true
+    HasBeenLoaded: false
   }
 }
 render(){
