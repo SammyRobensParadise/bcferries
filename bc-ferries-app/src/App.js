3d1bfc451b42ac1366220dec4e35ddc2c25cdfb9
Sammy Robens-Paradise <srobensparadise@gmail.com>
2019-08-22 21:27:51 -0700

    added node-sass and npm

--- bc-ferries-app/src/App.js
+++ bc-ferries-app/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 import React, {
   Component
 } from 'react';
 import { BrowserRouter, Route, Link } from "react-router-dom";
-
+import { version, Button } from "antd";
+import "antd/dist/antd.css";
 export default class AppHandler extends Component {
 constructor(props){
   super(props);
