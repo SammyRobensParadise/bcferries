770cab58a24d33fc284cf6f6f54cccf1d4dc61d7
Sammy Robens-Paradise <srobensparadise@gmail.com>
2019-09-15 22:25:00 -0700

    added firebase

--- bc-ferries-app/src/App.js
+++ bc-ferries-app/src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.scss';
 import textHandler from './__static_class__/-text-handler';
 import React, {
   Component
 } from 'react';
-import { BrowserRouter, Route, Link } from "react-router-dom";
-import { version, Button } from "antd";
+//import { BrowserRouter, Route, Link } from "react-router-dom";
+//import { version, Button } from "antd";
 import "antd/dist/antd.css";
 import Navbar from './components/global/-navigation-bar/Navbar'
 export default class AppHandler extends Component {
@@ -21,8 +20,8 @@ render(){
     <div>
       <Navbar/>
       <p>This is an  example of the default class</p>
-      <p>{textHandler._getData}</p>
-      {console.log(textHandler)}
+      <p>{textHandler.getUserData()}</p>
+      {console.log(textHandler,"in App")}
     </div>
   )
 }
--- bc-ferries-app/src/__static_class__/-text-handler.jsx
+++ bc-ferries-app/src/__static_class__/-text-handler.jsx
@@ -3,30 +3,28 @@ import React, {
   Component
 } from 'react';
 import "antd/dist/antd.css";
+import config from './config';
 import firebase from 'firebase';
 export default class textHandler {
 constructor(props){
   this.state = {
-    HasBeenLoaded: false
+    HasBeenLoaded: false,
+    developers: []
     
   }
-  var config = {
-    apiKey: "AIzaSyCCL-k7gaqudFe5o1Y78YCfYsYgJ27qMFU",
-    authDomain: "https://bc-ferries-dev.firebaseapp.com",
-    databaseURL: "https://bc-ferries-dev.firebaseio.com",
-    storageBucket: "gs://bc-ferries-dev.appspot.com/"
-  };
   firebase.initializeApp(config);
-  console.log(config)
+  console.log(config,"config")
+  };
+
 
   // Get a reference to the database service
- 
-}
-_getData = () => {
-    var database = firebase.database().ref('stringReferences');
-    console.log("text");
-database.on('value', function(snapshot) {
-    return database;
-});
-}
+  getUserData = () => {
+    console.log("called")
+    let ref = firebase.database().ref("/");
+    ref.on("value", snapshot => {
+      const state = snapshot.val();
+      console.log(state)
+      this.setState(state);
+    });
+  };
 }
